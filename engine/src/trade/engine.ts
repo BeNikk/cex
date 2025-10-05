1629dc68df9149ef1dda100c70b3fb00739d3a86
BeNikk <bhattnik442@gmail.com>
2025-10-05 11:45:29 +0530

    feat: buy logic match added

---
+++ engine/src/trade/engine.ts
@@ -0,0 +1,28 @@
+import fs from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+interface UserBalance {
+  [key: string]: {
+    available: number,
+    locked: number
+  }
+}
+
+export class Engine {
+  private orderBooks: any = [];
+  private balances: Map<string, UserBalance> = new Map();
+  constructor() {
+    let snapShot = null; //should come from db/s3
+    try {
+      if (process.env.SNAPSHOT) {
+        snapShot = fs.readFileSync('./snapshots.json');
+      }
+    }
+    catch (e) {
+      console.log("ERROR IN LOADING SNAPSHOT")
+    }
+    if (snapShot) {
+      const snapShotJson = JSON.parse(snapShot.toString());
+    }
+  }
+}
---
+++ engine/src/trade/orderBooks.ts
@@ -0,0 +1,88 @@
+
+export interface Order {
+  price: number;
+  quantity: number;
+  side: Side;
+  orderId: string;
+  filled: number;
+  userId: string;
+}
+enum Side {
+  BUY = "BUY",
+  SELL = "SELL"
+}
+
+//initialising an orderBook class for each market
+export class OrderBook {
+  bids: Order[];
+  asks: Order[];
+  baseAsset: string; // in SOL_USDC , SOL is the base 
+  quoteAsset: string = "USDC"; // USDC in SOL_USDC
+  lastTradeId: number;
+  currentPrice: number;
+
+  constructor(baseAsset: string, bids: Order[], asks: Order[], lastTradeId: number, currentPrice: number) {
+    this.bids = bids;
+    this.asks = asks;
+    this.lastTradeId = lastTradeId || 0;
+    this.currentPrice = currentPrice || 0;
+    this.baseAsset = baseAsset;
+
+  }
+  ticker() {
+    return `${this.baseAsset}_${this.quoteAsset}`;
+  }
+  getSnap() {
+    return {
+      baseAsset: this.baseAsset,
+      bids: this.bids,
+      asks: this.asks,
+      lastTradeId: this.lastTradeId,
+      currentPrice: this.currentPrice,
+    }
+  }
+
+  matchBuys(order: Order) {
+    const fills = [];
+    let executed = 0;
+    for (let i = 0; i < this.asks.length; i++) {
+      const ask = this.asks[i]!;
+      if (ask.price <= order.price && executed < order.quantity) {
+        const filledQty = Math.min((order.quantity - executed), ask.quantity);
+        executed += filledQty;
+        ask.filled += filledQty;
+        fills.push({
+          price: ask.price.toString(),
+          qty: filledQty,
+          tradeId: this.lastTradeId++,
+          otherUserId: ask.userId,
+          marketOrderId: ask.orderId
+        })
+      }
+    }
+    for (let i = 0; i < this.asks.length; i++) {
+      let ask = this.asks[i]!;
+      if (ask.filled === ask.quantity) {
+        this.asks.splice(i, 1);
+        i--;
+      }
+    }
+    return {
+      fills,
+      executed
+    };
+  }
+  matchSells() {
+
+  }
+  createOrder(order: Order) {
+    try {
+      if (order.side == "BUY") {
+      }
+    } catch (error) {
+      console.log("Error in creating order");
+      return;
+    }
+
+  }
+}
