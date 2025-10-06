7e1ba8e629b720f8c77deed7f9b2687a77f16e31
BeNikk <bhattnik442@gmail.com>
2025-10-06 19:24:08 +0530

    add:saving snapshots temp

--- engine/src/trade/engine.ts
+++ engine/src/trade/engine.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import dotenv from "dotenv";
+import { OrderBook } from "./orderBooks";
 dotenv.config();
 interface UserBalance {
   [key: string]: {
@@ -7,7 +8,7 @@ interface UserBalance {
     locked: number
   }
 }
-
+const BASE_CURRENCY = "USDC"
 export class Engine {
   private orderBooks: any = [];
   private balances: Map<string, UserBalance> = new Map();
@@ -23,7 +24,48 @@ export class Engine {
     }
     if (snapShot) {
       const snapShotJson = JSON.parse(snapShot.toString());
-
+      this.orderBooks = snapShotJson.orderbooks.map((o: any) => new OrderBook(o.baseAsset, o.bids, o.asks, o.lastTradeId, o.currentPrice));
+      this.balances = new Map(snapShotJson.balances);
     }
+    else {
+      this.orderBooks = [new OrderBook(`SOL`, [], [], 0, 0)];
+      this.setBaseBalances();
+    }
+    setInterval(() => {
+      this.saveSnapshot();
+    }, 1000 * 3);
   }
+
+  saveSnapshot() {
+    const snapshotSnapshot = {
+      orderbooks: this.orderBooks.map((o: any) => o.getSnapshot()),
+      balances: Array.from(this.balances.entries())
+    }
+    //should save to s3
+    fs.writeFileSync("./snapshots.json", JSON.stringify(snapshotSnapshot));
+  }
+  setBaseBalances() {
+    this.balances.set("1", {
+      [BASE_CURRENCY]: {
+        available: 50000,
+        locked: 0
+      },
+      "SOL": {
+        available: 50000,
+        locked: 0
+      }
+    });
+
+    this.balances.set("2", {
+      [BASE_CURRENCY]: {
+        available: 50000,
+        locked: 0
+      },
+      "SOL": {
+        available: 50000,
+        locked: 0
+      }
+    });
+  }
+
 }
